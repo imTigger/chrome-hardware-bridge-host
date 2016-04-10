3af6d68da95472b5a1137f2954e839867a13f0e4
Tiger Fok <tiger@tiger-workshop.com>
2016-04-10 20:29:21 +0800

    Compatibility fix

--- install_host.bat
+++ install_host.bat
@@ -2,4 +2,4 @@
 
 :: Change HKCU to HKLM if you want to install globally.
 :: %~dp0 is the directory containing this bat script and ends with a backslash.
-REG ADD "HKCU\Software\Google\Chrome\NativeMessagingHosts\com.tigerworkshop.chrome.hardwarebridge" /ve /t REG_SZ /d "%~dp0com.tigerworkshop.chrome.hardwarebridge-win.json" /f
+REG ADD "HKLM\Software\Google\Chrome\NativeMessagingHosts\com.tigerworkshop.chrome.hardwarebridge" /ve /t REG_SZ /d "%~dp0com.tigerworkshop.chrome.hardwarebridge-win.json" /f
